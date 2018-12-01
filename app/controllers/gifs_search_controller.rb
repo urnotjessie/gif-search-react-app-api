06c75591c312fa2dad6b7dc0ca02002bf8d3550f
urnotjessie <zg2220@columbia.edu>
2018-12-01 16:22:45 -0500

    modify search controller to take search_term from client side

--- app/controllers/gifs_search_controller.rb
+++ app/controllers/gifs_search_controller.rb
@@ -1,9 +1,8 @@
 class GifsSearchController < ApplicationController
   def index
-    search_term = params[:search]
-
+    search_term = params[:q]
     @gifs = RestClient::Request.execute(:method => "get",
-        :url => "http://api.giphy.com/v1/gifs/search?q=" + search_term+ "&api_key=" + ENV['API_KEY'])
+        :url => "http://api.giphy.com/v1/gifs/search?q=#{search_term}&api_key=" + ENV['API_KEY'])
         render json: @gifs
   end
 end
