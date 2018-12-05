fa1bf2eb5bd011b10293ef2fa706141ecacc5f29
urnotjessie <zg2220@columbia.edu>
2018-12-05 13:09:48 -0500

    add trending#show to show individual gif

--- app/controllers/gifs_trending_controller.rb
+++ app/controllers/gifs_trending_controller.rb
@@ -5,5 +5,12 @@ class GifsTrendingController < ApplicationController
         :url => "http://api.giphy.com/v1/gifs/trending?api_key=" + ENV['API_KEY'])
         render json: @gifs
   end
-  
+
+  def show
+    target_id = params[:id]
+    @gif = RestClient::Request.execute(:method => "get",
+        :url => "http://api.giphy.com/v1/gifs/#{target_id}?api_key=" + ENV['API_KEY'])
+        render json: @gif
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'api/search', to: 'gifs_search#index'
+  get 'api/trending/:id', to: 'gifs_trending#show'
   get 'api/trending', to: 'gifs_trending#index'
+
 end
