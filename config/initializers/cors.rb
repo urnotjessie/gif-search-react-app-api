a5851d9c271afd724d1386405a6c5350b7ca6507
urnotjessie <zg2220@columbia.edu>
2018-12-06 21:59:42 -0500

    update new name of react front end url in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'https://desolate-inlet-45447.herokuapp.com']
+    origins ['localhost:3000', 'https://gif-search-react-app.herokuapp.com']
 
     resource '*',
       headers: :any,
