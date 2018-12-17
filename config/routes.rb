f9c508a2b86d86e4b1ef05b21bdc2e18def325e3
urnotjessie <zg2220@columbia.edu>
2018-12-17 13:30:53 -0500

    update routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'api/search', to: 'gifs_search#index'
+
   get 'api/trending/:id', to: 'gifs_trending#show'
   get 'api/trending', to: 'gifs_trending#index'
 
-  namespace :api do
-    resource :favorite
-  end
+  get 'api/favorite', to: 'gifs_favorite#index'
+  post 'api/favorite', to: 'gifs_favorite#create'
 
 end
